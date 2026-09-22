ecd9c64ba21fb71de1d7938d1eb195241a78c95c
Trương Võ Trọng Nhân <hoangy882@gmail.com>
2026-09-23 02:56:16 +0700

    Xoa cac file script va scratch tam thoi

--- backend/tests/CulinaryBlog.IntegrationTests/ConcurrencyScratch.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Threading.Tasks;
-using CulinaryBlog.Infrastructure;
-using CulinaryBlog.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
-
-// I'm going to just write a simple class to demonstrate the concept, but wait, I can't easily compile it unless I put it in a project.
